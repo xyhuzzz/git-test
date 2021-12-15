23bd088af42f4aa51d67cca5c5388f8bd612f7ec
Layne <1219088675@qq.com>
2021-12-15 19:04:29 +0800

    forth commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -1,9 +1,9 @@
 package com.atguigu;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git");
+        System.out.println("hello git3");
     }
 }
